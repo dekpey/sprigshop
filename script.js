1e03025f2cda041f5491bd0b21cbcc3e867cccfa
Deki Pelmo <dekipelmo@dekis-mbp.home>
2021-10-17 20:44:41 +1100

    html and css are done, need to add js

---
+++ script.js
@@ -0,0 +1,19 @@
+// const toggle = document.getElementsByClassName('toggle')[0]
+// const item = document.getElementsByClassName('item')[0]
+
+// toggle.addEventListener('click', ()=> {
+//     item.classList.toggle('active')
+// })
+
+// $(function(){
+//    $(".toggle").on("click", function(){
+//        if($(".item").hasClass("active")){
+//            $(".item").removeClass("active");
+//        } 
+//        else {
+//            $(".item").addClass("active");
+//        }
+//    })
+//   });
+
+
